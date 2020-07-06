80df64547cd5b4ae1d4892da51c63f2b29adcf6b
Gouzhong1223 <1162864960@qq.com>
2020-07-06 22:13:37 +0800

    init

---
+++ src/main/java/org/gouzhong1223/App.java
@@ -0,0 +1,13 @@
+package org.gouzhong1223;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

22847e8683bb827c65d684ddaf2fc948219f51cf
Gouzhong1223 <1162864960@qq.com>
2020-07-08 23:26:48 +0200

    add Spring Boot netty demo

---
+++ springboot-netty-demo/src/main/java/org/gouzhong1223/springbootnettydemo/SpringbootNettyDemoApplication.java
@@ -0,0 +1,13 @@
+package org.gouzhong1223.springbootnettydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNettyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNettyDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-netty-demo/src/test/java/org/gouzhong1223/springbootnettydemo/SpringbootNettyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gouzhong1223.springbootnettydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNettyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

9cf6b29f26b6695725e8cd43863b2f683911ebc7
Gouzhong1223 <1162864960@qq.com>
2020-07-08 23:40:29 +0200

    add demo code

---
+++ springboot-netty-demo/src/main/java/org/gouzhong1223/springbootnettydemo/initializer/NettyServerHandlerInitializer.java
@@ -0,0 +1,28 @@
+package org.gouzhong1223.springbootnettydemo.initializer;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author : Gouzhong
+ * @company : www.gouzhong1223.com
+ * @Description :
+ * @date : create by QingSong in 2020-07-08 23:34
+ * @email : 1162864960@qq.com
+ */
+public class NettyServerHandlerInitializer implements ChannelHandler {
+    @Override
+    public void handlerAdded(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext channelHandlerContext) throws Exception {
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable) throws Exception {
+
+    }
+}

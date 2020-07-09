0155b931e1bb49969bbb023e93df043cb0bb2245
Gouzhong1223 <1162864960@qq.com>
2020-07-09 17:44:21 +0800

    Add SpringBoot Netty WebSocket Demo Code

---
+++ springboot-netty-websocket-demo/src/main/java/org/gouzhong1223/springbootnettywebsocketdemo/SpringbootNettyWebsocketDemoApplication.java
@@ -0,0 +1,13 @@
+package org.gouzhong1223.springbootnettywebsocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNettyWebsocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNettyWebsocketDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-netty-websocket-demo/src/test/java/org/gouzhong1223/springbootnettywebsocketdemo/SpringbootNettyWebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gouzhong1223.springbootnettywebsocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNettyWebsocketDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
